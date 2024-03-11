9c734357dadd87dc8d614c94fb571dfc188d2f4f
sidharth72 <sidharthan261@gmail.com>
2024-03-11 18:15:04 +0530

    Implemented Chatbot Initial Stage

--- JobSumv2/response_generator.py
+++ JobSumv2/response_generator.py
@@ -3,6 +3,16 @@ import getpass
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
+import time
+import streamlit as st
+import config
 
 
-def chat_with_gemini(system_message, prompt):
+model = ChatGoogleGenerativeAI(model = 'gemini-pro', convert_system_message_to_human=True)
+
+def chat_with_gemini(prompt):
+    result = model.invoke(prompt)
+    
+    for word in result.content.split():
+        yield word + " "
+        time.sleep(0.05)
--- JobSumv2/ui.py
+++ JobSumv2/ui.py
@@ -1,20 +1,32 @@
 import streamlit as st
 from generate_df import generate_dataframe
 from download_csv import get_table_download_link
+from response_generator import chat_with_gemini
 
+# st.set_page_config(
+#     page_title = 'JobSum'
+# )
 
 def extraction_tab():
-    st.title("Data Extraction")
+    st.title("Extract Data")
+    st.write('<div style="opacity: 0.7;">Extract data from various sites by providing necessary details.</div>', unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
     site_name = st.selectbox("Select Site Name", ["Indeed", "Linkedin", "Zip_recruiter", "Glassdoor"])
     location = st.text_input("Location", key="location_input", placeholder='Enter Location to search for')
     search_term = st.text_input("Search Term", key="search_term_input", placeholder='Enter Job Role, Eg: Data Scientist')
-    results_wanted = st.number_input("Results Wanted", min_value=1, step=1)
+    results_wanted = st.number_input("Results Wanted", min_value=1, max_value=500, step=1)
     if site_name == "Indeed":
         country = st.text_input("Country", key="country_input", placeholder='Enter Country')
     else:
         country = st.text_input("Country", key="country_input", placeholder='Enter Country', disabled=True)
+    
+    # Check if 'desc_string' is not in session_state, initialize it
+    if 'desc_string' not in st.session_state:
+        st.session_state.desc_string = ""
+
     if st.button("Extract Data"):
         df = generate_dataframe(site_name, search_term, location, results_wanted, country)
+        st.session_state.desc_string = '\n'.join(f'{i + 1}. {desc}' for i, desc in enumerate(df['description'][:results_wanted], start=0))
         st.dataframe(df)
         st.markdown(get_table_download_link(df), unsafe_allow_html=True)
 
@@ -28,28 +40,24 @@ def api_key_interface():
 
 
 def chat_tab():
-    st.title("Chat Interface")
+    st.title("Chat With Gemini")
 
-    if st.session_state.api_key is None:
-        api_key_interface()
-    else:
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
 
-        if 'messages' not in st.session_state:
-            st.session_state.messages = []
+    for message in st.session_state.messages:
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
 
-        for message in st.session_state.messages:
-            with st.chat_message(message['role']):
-                st.markdown(message['content'])
+    if prompt := st.chat_input("What is up?"):
+        st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role':'user', 'content':prompt})
+        response = f"Echo: {prompt}"
 
-        if prompt := st.chat_input("What is up?"):
-            st.chat_message('user').markdown(prompt)
-            st.session_state.messages.append({'role':'user', 'content':prompt})
-            response = f"Echo: {prompt}"
+        with st.chat_message('Assistant'):
+            response = st.write_stream(chat_with_gemini(prompt))
 
-            with st.chat_message('Assistant'):
-                st.markdown(response)
-
-            st.session_state.messages.append({'role':'assistant', 'content':response})
+        st.session_state.messages.append({'role':'assistant', 'content':response})
 
 def visualization_tab():
     st.title("Data Visualization")
